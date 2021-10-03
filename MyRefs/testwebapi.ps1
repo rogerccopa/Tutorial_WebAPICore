89410865d4fb1f018d7aa32561e40ced309a5347
Roger Ccopa <rogerccopa@delllaptop.com>
2021-10-03 15:27:21 -0500

    Initial Web API example

---
+++ MyRefs/testwebapi.ps1
@@ -0,0 +1,14 @@
+﻿$response = Invoke-RestMethod -Uri "https://localhost:44390/api/tickets" -Method Get
+$response
+
+$response = Invoke-RestMethod -Uri "https://localhost:44390/api/tickets/123" -Method Get
+$response
+
+$response = Invoke-RestMethod -Uri "https://localhost:44390/api/tickets" -Method Post
+$response
+
+$response = Invoke-RestMethod -Uri "https://localhost:44390/api/tickets" -Method Put
+$response
+
+$response = Invoke-RestMethod -Uri "https://localhost:44390/api/tickets/456" -Method Delete
+$response
---
+++ PlatformDemo/Controllers/TicketsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformDemo.Controllers
+{
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/tickets")]
+        public IActionResult Get()
+        {
+            return Ok("Reading all the tickets");
+        }
+
+        [HttpGet]
+        [Route("api/tickets/{id}")]
+        public IActionResult GetById(int Id)
+        {
+            return Ok($"Reading ticket #{Id}");
+        }
+
+        [HttpPost]
+        [Route("api/tickets")]
+        public IActionResult Create(int Id)
+        {
+            return Ok("Creating a ticket");
+        }
+
+        [HttpPut]
+        [Route("api/tickets")]
+        public IActionResult Update(int Id)
+        {
+            return Ok("Updating a ticket");
+        }
+
+        [HttpDelete]
+        [Route("api/tickets/{id}")]
+        public IActionResult Delete(int Id)
+        {
+            return Ok("Deleting a ticket");
+        }
+    }
+}
