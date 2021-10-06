c39babebbc18a0bb7d226b6c38227f6dc7ffead5
Roger Ccopa <rogerccopa@delllaptop.com>
2021-10-06 10:34:31 -0500

    ValidationAttribute

--- PlatformDemo/Models/Ticket.cs
+++ PlatformDemo/Models/Ticket.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using PlatformDemo.ModelValidations;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PlatformDemo.Models
 {
     public class Ticket
     {
-        public int TicketId { get; set; }
-        public int ProjectId { get; set; }
+        public int? TicketId { get; set; }
+
+        [Required]
+        public int? ProjectId { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Owner { get; set; }
+
+        [Ticket_EnsureDueDateForTicketOwner]
+        public DateTime? DueDate { get; set; }
     }
 }
