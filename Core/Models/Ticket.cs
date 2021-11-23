a7c3e5bc54cd1bee191cf7b16168f237e2dd4120
Roger Ccopa <rogerccopa@delllaptop.com>
2021-11-23 17:01:28 -0500

    Decoupled Model classes

--- Core/Models/Ticket.cs
+++ Core/Models/Ticket.cs
@@ -1,23 +1,29 @@
-﻿using PlatformDemo.ModelValidations;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Text;
 
-namespace PlatformDemo.Models
+namespace Core.Models
 {
-    public class Ticket
+     public class Ticket
     {
         public int? TicketId { get; set; }
 
         [Required]
         public int? ProjectId { get; set; }
+
         [Required]
+        [StringLength(100)]
         public string Title { get; set; }
         public string Description { get; set; }
+
+        [StringLength(50)]
         public string Owner { get; set; }
 
-        [Ticket_EnsureDueDateForTicketOwner]
-        [Ticket_EnsureDueDateInTheFutureAttr]
+        public DateTime? ReportDate { get; set; }
+
         public DateTime? DueDate { get; set; }
-        public DateTime? EnteredDate { get; set; }
+
+        public Project Project { get; set; }
     }
 }
